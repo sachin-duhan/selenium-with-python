9c09b857d38241edb416827cda199c1e462f935b
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-11 10:11:06 +0530

    record keeper

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import random
 import names
 from selenium import webdriver
 import time
+import os
 from selenium.webdriver.common.keys import Keys
+
 
 POST_URL = "https://app.biorender.com/contest/profile/5dc321790d5929007dfbf7bb-satish-poojary/s-5ed39d568563ce00af3b4cc3-tumor-microenvironment-drives-cancer-progression"
 SIGN_UP = "https://app.biorender.com/user/signup"
 
+
 class User():
     f = names.get_first_name()
     l = names.get_last_name()
-    n = random.randint(1,10) * random.randint(200,2000)
+    n = random.randint(1, 10) * random.randint(200, 2000)
+
 
 vote_count_needed = 1
+
 
 def vote():
     driver = webdriver.Chrome()
@@ -23,11 +28,16 @@ def vote():
     driver.maximize_window()
     driver.get(SIGN_UP)
     myUser = User()
-    myUser.n *= random.randint(2,10)
+    myUser.n *= random.randint(2, 10)
     sign_up_form = {'firstName': myUser.f, 'lastName': myUser.l,
                     'email': myUser.f + myUser.l + str(myUser.n) + '@gmail.com',
                     'password': 'password'}
-    print("user email is - ",sign_up_form["email"])
+    
+    file1 = open("users.txt", "a")  # append mode
+    file1.write(sign_up_form["email"])
+    file1.close()
+    
+    print("user email is - ", sign_up_form["email"])
     driver.implicitly_wait(15)
 
     # sign up form iteration
@@ -59,15 +69,18 @@ def vote():
         "//button[@class='button vote-button__container outline contest-preview-modal__action']")
     for el in someList:
         el.click()
-    # driver.close()
+    time.sleep(3)
+    driver.close()
+
 
 print("================================================")
 print("wokring...")
-for i in range(vote_count_needed):
-    try:
-        vote()
-    except:
-        vote()
 
-print("================================")
+for i in range(vote_count_needed):
+    print("Programmed to be executed", vote_count_needed, "times")
+    print("User email are as follows - ")
+    print("================================================================")
+    vote()
+
+print("================================================")
 print("successfully completed")
